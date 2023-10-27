3e7a268bfcb0a4090e15947bb23973786b3ef1a3
yasid <yasidalmubarok@gmail.com>
2023-10-27 22:07:58 +0700

    [Aggregate Category] fixed: aggregate one to many

--- repository/category_repo/aggregate.go
+++ repository/category_repo/aggregate.go
@@ -5,8 +5,6 @@ import (
 	"time"
 )
 
-
-
 type task struct {
 	Id          int       `json:"id"`
 	Title       string    `json:"title"`
@@ -21,34 +19,47 @@ type CategoryTask struct {
 	Category entity.Category
 	Task     entity.Task
 }
+
 type CategoryTaskMapped struct {
 	Id        int       `json:"id"`
 	Type      string    `json:"type"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-	Tasks     task      `json:"Task"`
+	Tasks     []task    `json:"Task"`
 }
 
 func (ctm *CategoryTaskMapped) HandleMappingCategoryWithTask(categoryTask []CategoryTask) []CategoryTaskMapped {
-	categoryTasksMapped := []CategoryTaskMapped{}
+	categoryTasksMap := make(map[int]CategoryTaskMapped)
 
 	for _, eachCategoryTask := range categoryTask {
-		categoryTaskMapped := CategoryTaskMapped{
-			Id:        eachCategoryTask.Category.Id,
-			Type:      eachCategoryTask.Category.Type,
-			CreatedAt: eachCategoryTask.Category.CreatedAt,
-			UpdatedAt: eachCategoryTask.Category.UpdatedAt,
-			Tasks: task{
-				Id:          eachCategoryTask.Task.Id,
-				Title:       eachCategoryTask.Task.Title,
-				Description: eachCategoryTask.Task.Description,
-				UserId:      eachCategoryTask.Task.UserId,
-				CategoryId:  eachCategoryTask.Task.CategoryId,
-				CreatedAt:   eachCategoryTask.Task.CreatedAt,
-				UpdatedAt:   eachCategoryTask.Task.UpdatedAt,
-			},
+		categoryId := eachCategoryTask.Category.Id
+		categoryTaskMap, exists := categoryTasksMap[categoryId]
+		if !exists {
+			categoryTaskMap = CategoryTaskMapped{
+				Id:        eachCategoryTask.Category.Id,
+				Type:      eachCategoryTask.Category.Type,
+				CreatedAt: eachCategoryTask.Category.CreatedAt,
+				UpdatedAt: eachCategoryTask.Category.UpdatedAt,
+			}
 		}
-		categoryTasksMapped = append(categoryTasksMapped, categoryTaskMapped)
+
+		taskItem := task{
+			Id:          eachCategoryTask.Task.Id,
+			Title:       eachCategoryTask.Task.Title,
+			Description: eachCategoryTask.Task.Description,
+			UserId:      eachCategoryTask.Task.UserId,
+			CategoryId:  eachCategoryTask.Task.CategoryId,
+			CreatedAt:   eachCategoryTask.Task.CreatedAt,
+			UpdatedAt:   eachCategoryTask.Task.UpdatedAt,
+		}
+		categoryTaskMap.Tasks = append(categoryTaskMap.Tasks, taskItem)
+		categoryTasksMap[categoryId] = categoryTaskMap
 	}
-	return categoryTasksMapped
+
+	categoryTasks := []CategoryTaskMapped{}
+	for _, categoryTask := range categoryTasksMap {
+		categoryTasks = append(categoryTasks, categoryTask)
+	}
+
+	return categoryTasks
 }
